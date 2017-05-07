68d4bc5f08849d3171f0254d9eecec4fa8cad595
nublikaska <nublikaska@gmail.com>
2017-05-07 13:32:43 +0500

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SendMessageFromSocket(11000, "1276");
+            }
+            catch (Exception ex)
+            {
+            }
+            /*catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }*/
+        }
+
+        static void SendMessageFromSocket(int port, string PoleChess)
+        {
+            // Буфер для входящих данных
+            byte[] bytes = new byte[1024];
+
+            // Соединяемся с удаленным устройством
+
+            // Устанавливаем удаленную точку для сокета
+            IPHostEntry ipHost = Dns.GetHostEntry("192.168.42.194");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Соединяем сокет с удаленной точкой
+            sender.Connect(ipEndPoint);
+
+            string message = PoleChess;
+
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+
+            // Отправляем данные через сокет
+            int bytesSent = sender.Send(msg);
+
+            // Получаем ответ от сервера
+            int bytesRec = sender.Receive(bytes);
+
+            Console.WriteLine("\nОтвет от сервера: {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+            // Используем рекурсию для неоднократного вызова SendMessageFromSocket()
+            //if (message.IndexOf("<TheEnd>") == -1)
+                //SendMessageFromSocket(port, 1, 0, 7, 6);
+
+            // Освобождаем сокет
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+            string a;
+            a = Console.ReadLine();
+        }
+    }
+}
